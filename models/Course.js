f5710e3216ea9d403b63b515a2b5624d050ca4ed
Mohit <135839490+xxmnkkk@users.noreply.github.com>
2024-01-14 10:21:58 +0530

    Update Course.js

--- models/Course.js
+++ models/Course.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const courseSchema = new mongoose.Schema({
   // name: String (required)
+  name: {
+    type: String,
+    required: true
+  },
   // description: String
+  description: String
 });
 
 const Course = mongoose.model('Course', courseSchema);

23c56a5da09de7edf897b2f2becadb31e12a21a9
Mohit <135839490+xxmnkkk@users.noreply.github.com>
2024-01-14 10:24:05 +0530

    Update Grade.js

--- models/Grade.js
+++ models/Grade.js
@@ -2,8 +2,21 @@ const mongoose = require('mongoose');
 
 const gradeSchema = new mongoose.Schema({
   // studentId: ObjectId (required)
+  studentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
   // courseId: ObjectId (required)
+  courseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
   // grade: String (required, enum: ['A', 'B', 'C', 'D', 'F'])
+  grade: {
+    type: String,
+    required: true,
+    enum: ['A', 'B', 'C', 'D', 'F']
+  }
 });
 
 const Grade = mongoose.model('Grade', gradeSchema);

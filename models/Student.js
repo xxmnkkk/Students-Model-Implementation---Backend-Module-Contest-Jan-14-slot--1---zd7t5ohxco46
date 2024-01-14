1d7f35831ea8a537cbdbdb12d03ffe38468f7797
Mohit <135839490+xxmnkkk@users.noreply.github.com>
2024-01-14 10:27:42 +0530

    Update Student.js

--- models/Student.js
+++ models/Student.js
@@ -2,12 +2,41 @@ const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
   // firstName: String (required)
+  firstName: {
+    type: String,
+    required: true
+  },
   // lastName: String (required)
+  lastName: {
+    type: String,
+    required: true
+  },
   // dateOfBirth: Date (required)
+  dateOfBirth: {
+    type: Date,
+    required: true
+  },
   // email: String (required, unique)
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
   // courses: Array of Course IDs
+  courses: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course'
+  }],
   // grades: Array of Grade Objects
+  grades: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Grade'
+  }],
   // attendance: Array of Attendance Objects
+  attendance: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Attendance'
+  }]
 });
 
 const Student = mongoose.model('Student', studentSchema);
